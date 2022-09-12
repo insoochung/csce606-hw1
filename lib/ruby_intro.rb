159827f840bbcef22e736c93adb3a71b701665c7
Insoo Chung <insoooo.chung@gmail.com>
2022-09-11 21:00:59 -0500

    Implement part 1, 2 and 3 specs

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,34 +3,66 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  return arr.sum
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  return arr.max(2).sum
 end
 
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+  arr.permutation(2).to_a.each do |(x, y)|
+    return true if x + y == number
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return 'Hello, ' + name
 end
 
 def starts_with_consonant?(string)
-  # YOUR CODE HERE
+  return false if string.length == 0 or !string[0].match?(/[[:alpha:]]/)
+  return !['a', 'e', 'i', 'o', 'u'].include?(string[0].downcase)
 end
 
 def binary_multiple_of_4?(string)
-  # YOUR CODE HERE
+  return false if !string.match(/\A(0|1)+\Z/)
+  return string.to_i(2) % 4 == 0
 end
 
 # Part 3
 
 # Object representing a book
+# Object representing a book
 class BookInStock
-  # YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError.new 'Invalid ISBN (empty string)' if isbn == ''
+    raise ArgumentError.new 'Invalid price (0 or smaller value)' if price <= 0
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def isbn
+    return @isbn
+  end
+
+  def price
+    return @price
+  end
+
+  def isbn=(isbn)
+    return @isbn = isbn
+  end
+
+  def price=(price)
+    return @price = price
+  end
+
+  def price_as_string
+    return "$%0.2f" % @price
+  end
 end
